ec50b643274480f914571c2ee8dd91841d038f5f
murillojoaquim <murillojalves@hotmail.com>
2021-09-17 18:49:41 -0300

    Calculo Area Triangulo Retangulo

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Digite o valor da base do triângulo");
+            string Base = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\nAgora, digite o valor da altura:");
+            string Altura = Console.ReadLine();
+
+            double areaTriangulo = (Convert.ToDouble(Base) * Convert.ToDouble(Altura)) / 2;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"\nTotal área = {areaTriangulo}");
+
+            Console.ReadKey();
+            Console.ResetColor();
+        }
+    }
+}
